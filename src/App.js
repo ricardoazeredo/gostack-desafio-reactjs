c89810c402d01215de1973dc53e3a7aedbac1904
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2020-11-06 18:04:56 -0300

    Creating a front-end repository with ReactJS from the GoStack Challenge

--- src/App.js
+++ src/App.js
@@ -1,31 +1,56 @@
-import React from "react";
+import React, {useState , useEffect} from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories,setRepositories] = useState([]);
+
+  useEffect(() =>{
+    api.get('repositories').then(response => {
+      
+      setRepositories(response.data) 
+    })
+  },[]);
+
   async function handleAddRepository() {
-    // TODO
+    
+    const response = await api.post('repositories',{
+      title: 'Ricardo',
+      url: 'ricardoazeredo/gostack-desafio-reactjs',
+      techs: ['node', 'reactjs'],
+    });
+
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    // TODO         
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(
+      repository => repository.id != id, 
+    ));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+          {repositories.map(respository => (
+            <li key={respository.id}>
+              {respository.title}
+              
+              <button onClick={() => handleRemoveRepository(respository.id)}>
+                Remover
+              </button>
+            </li>
+            )          
+          )}         
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
-
 export default App;
